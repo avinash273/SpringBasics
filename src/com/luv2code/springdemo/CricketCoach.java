396c74714cc8d5056eaa42473df9379ff40c65aa
avinash273 <avinash.shanker@mavs.uta.edu>
2021-02-12 12:31:34 -0600

    literal Injection

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -4,6 +4,28 @@ public class CricketCoach implements Coach{
 
     private FortuneService fortuneService;
 
+    //add new fields for email and team
+    private String emailAddress;
+    private String team;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("Cricket coach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("Cricket coach: inside setter method - setTeam");
+        this.team = team;
+    }
+
     //setter method
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
